546669681e69d780b3919e27b384b693465d809d
Sergey Matviychuk <35134108+Daarien@users.noreply.github.com>
2020-02-01 22:00:22 +0300

    chore: update deps

--- src/jwt.ts
+++ src/jwt.ts
@@ -1,7 +1,7 @@
-import { Secret, SignOptions } from "jsonwebtoken";
-interface JwtAuthPayload {}
+import { secretType } from "express-jwt";
+import { SignOptions } from "jsonwebtoken";
 
-export const secretKey = "verySecretKey";
+export const secretKey: secretType = "verySecretKey";
 export const signOptions: SignOptions = {
   issuer: "Boss",
   expiresIn: "24h"
